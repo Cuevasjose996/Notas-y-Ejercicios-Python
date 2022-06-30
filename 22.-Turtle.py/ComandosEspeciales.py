3dd655ff2fb002c6003cacc44f844f6aec264ae1
CRUZ <cmijcuevas@outlook.com>
2022-06-30 02:07:58 -0500

    se agrega carpeta turtle

---
+++ 22.-Turtle.py/ComandosEspeciales.py
@@ -0,0 +1,18 @@
+import turtle
+
+s= turtle.Screen()
+t= turtle.Turtle()
+
+t.speed(10)
+t.circle(10)
+t.circle(50)
+t.dot(30)
+
+t.hideturtle()
+t.circle(40)
+t.showturtle()
+t.circle(100)
+
+t.setx(100)
+t.sety(-500)
+turtle.done()
---
+++ 22.-Turtle.py/Personalizar.py
@@ -0,0 +1,9 @@
+import turtle
+
+s= turtle.Screen()
+t= turtle.Turtle()
+
+s.bgcolor("red")
+s.title("Proyecto 1")
+
+turtle.done()
